502218f3c701a8bcd87c325e8260d2eb72c622cc
Sakiskid <tylerdrew.home@gmail.com>
2020-09-08 22:52:39 -0500

    using js file for data until I can learn Node. added first apparel to data, Bowler Hat

---
+++ JSON-Data/apparel.js
@@ -0,0 +1,22 @@
+var apparelData = [
+    {
+        name: "Bowler Hat",
+        description: "A good-looking hat which projects a slightly urban, money-loving style. Improves social impact.",
+        type: "Gear",
+        apparelType: "Clothing",
+        clothingType: "Headgear",
+        isDLC: "false",
+        researchRequirement: "Complex Clothing",
+        fabric: "true",
+        leathery: "false",
+        protection_sharp: 20,
+        protection_blunt: 20,
+        protection_heat: 20,
+        insulation_heat: 40,
+        insulation_cold: 10,
+        coverage: ["Head", "Left Ear", "Right Ear"],
+        layer: "Overhead",
+        workToMake: 30,
+        resourcesToMake: 20
+    }
+]
